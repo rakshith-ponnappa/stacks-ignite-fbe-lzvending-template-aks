4d05bb309ca257092d5c9d6064dcf2ed1d72f34a
Ben Heryet <ben.heryet@ensono.com>
2025-08-11 17:19:52 +0100

    Tweaks

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.110"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-
 resource "azurerm_resource_group" "rg_vnet" {
   name     = var.rg_name
   location = var.location
@@ -33,7 +18,6 @@ resource "azurerm_virtual_network" "vnet" {
   tags                = var.tags
 }
 
-# (Optional) create one or more subnets
 resource "azurerm_subnet" "workload" {
   name                 = "snet-workload"
   resource_group_name  = azurerm_resource_group.rg_vnet.name
@@ -41,8 +25,6 @@ resource "azurerm_subnet" "workload" {
   address_prefixes     = [var.subnet_prefix]
 }
 
-# --- Connect VNet to the existing vWAN Hub ---
-# If you used the data.azurerm_virtual_hub approach above, swap the virtual_hub_id.
 resource "azurerm_virtual_hub_connection" "vhub_conn" {
   name                      = "${var.vnet_name}-to-vhub"
   virtual_hub_id            = data.terraform_remote_state.prd_eastus2_connectivity.outputs.virtual_hub_resource_id
