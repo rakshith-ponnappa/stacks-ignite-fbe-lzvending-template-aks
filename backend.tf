2e583c7551f41a98fb4d8b4e6c1ad765afc958cb
Ben Heryet <ben.heryet@ensono.com>
2025-09-09 15:00:34 +0100

    Fixes

--- backend.tf
+++ backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
+    use_azuread_auth=true
     #storage_account_name = "" # set at init time
     #container_name       = "" # set at init time
     #key                  = "" # set at init time
