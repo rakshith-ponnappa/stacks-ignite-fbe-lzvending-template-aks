0933bdf10bcae9aa7adabdb22fdac63deb228220
Ben Heryet <ben.heryet@ensono.com>
2025-09-09 17:24:02 +0100

    Fixes

--- data_remote_state.tf
+++ data_remote_state.tf
@@ -14,17 +14,17 @@ data "terraform_remote_state" "prd_eastus2_connectivity" {
   }
 }
 
-data "terraform_remote_state" "prd_centralus_connectivity" {
-  backend   = "azurerm"
-  workspace = terraform.workspace
-  config = {
-    use_azuread_auth = true
-    tenant_id        = "d483ed84-f7bf-4078-a58f-fb250feccf8f"
-    subscription_id  = "cea31a73-c42c-4b17-b83d-c629142bf942"
+# data "terraform_remote_state" "prd_centralus_connectivity" {
+#   backend   = "azurerm"
+#   workspace = terraform.workspace
+#   config = {
+#     use_azuread_auth = true
+#     tenant_id        = "d483ed84-f7bf-4078-a58f-fb250feccf8f"
+#     subscription_id  = "cea31a73-c42c-4b17-b83d-c629142bf942"
     
-    storage_account_name = "steefbecuscontfstate"
-    container_name       = "tfstate"
-    key                  = "connectivity/coreenv:prd"
-    use_azuread_auth     = true
-  }
-}
+#     storage_account_name = "steefbecuscontfstate"
+#     container_name       = "tfstate"
+#     key                  = "connectivity/coreenv:prd"
+#     use_azuread_auth     = true
+#   }
+# }
