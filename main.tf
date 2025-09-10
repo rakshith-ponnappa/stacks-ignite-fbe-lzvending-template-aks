041fca5d206393f4275c601c45654293260f9024
Ben Heryet <ben.heryet@ensono.com>
2025-09-10 10:22:53 +0100

    Tidy

--- main.tf
+++ main.tf
@@ -27,19 +27,7 @@ resource "azurerm_subnet" "workload" {
 
 resource "azurerm_virtual_hub_connection" "vhub_conn" {
   name                      = "${var.vnet_name}-${var.subscription_alias_name}-${var.environment}-to-vhub"
-  virtual_hub_id            = data.terraform_remote_state.prd_eastus2_connectivity.outputs.virtual_hub_resource_id #todo spn access to remote state?
+  virtual_hub_id            = data.terraform_remote_state.prd_eastus2_connectivity.outputs.virtual_hub_resource_id
   #virtual_hub_id            = var.virtual_hub_id
   remote_virtual_network_id = azurerm_virtual_network.vnet.id
-
-  # internet_security_enabled = true
-
-  # routing {
-  #   associated_route_table_id = data.terraform_remote_state.core_network.outputs.default_route_table_id
-  #   propagated_route_table {
-  #     route_table_ids = [
-  #       data.terraform_remote_state.core_network.outputs.default_route_table_id
-  #     ]
-  #     labels = ["default"]
-  #   }
-  # }
 }
